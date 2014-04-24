bd20e2fca903e334f648ec17b6476f75f3049f41
haohailong <haohailong@gmail.com>
2014-04-25 01:13:40 +0200

    评论页汉化。

--- comments.php
+++ comments.php
@@ -9,7 +9,7 @@
 			<h2 class="comments-title">
 			
 				<?php echo count($wp_query->comments_by_type['comment']) . ' ';
-				echo _n( 'Comment' , 'Comments' , count($wp_query->comments_by_type['comment']), 'wilson' ); ?>
+				echo _n( '条评论' , '条评论' , count($wp_query->comments_by_type['comment']), 'wilson' ); ?>
 				
 			</h2>
 	
@@ -44,9 +44,9 @@
 			
 				<div class="comment-nav-below" role="navigation">
 									
-					<div class="post-nav-older"><?php previous_comments_link( __( '&laquo; Older<span> Comments</span>', 'wilson' ) ); ?></div>
+					<div class="post-nav-older"><?php previous_comments_link( __( '&laquo; <span> 较早的评论</span>', 'wilson' ) ); ?></div>
 					
-					<div class="post-nav-newer"><?php next_comments_link( __( 'Newer<span> Comments</span> &raquo;', 'wilson' ) ); ?></div>
+					<div class="post-nav-newer"><?php next_comments_link( __( '<span> 较新的评论</span> &raquo;', 'wilson' ) ); ?></div>
 					
 					<div class="clear"></div>
 					
@@ -60,14 +60,14 @@
 	
 	<?php if ( ! comments_open() && !is_page() ) : ?>
 	
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'wilson' ); ?></p>
+		<p class="nocomments"><?php _e( '该页评论已关闭。', 'wilson' ); ?></p>
 		
 	<?php endif; ?>
 	
 	<?php $comments_args = array(
 	
 		'comment_notes_before' => 
-			'<p class="comment-notes">' . __( 'Your email address will not be published.', 'wilson' ) . '</p>',
+			'<p class="comment-notes">' . __( '你的 Email 地址不会被公开。', 'wilson' ) . '</p>',
 	
 		'comment_field' => 
 			'<p class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="6" required>' . '</textarea></p>',
@@ -76,13 +76,13 @@
 		
 			'author' =>
 				'<p class="comment-form-author">' .
-				'<input id="author" name="author" type="text" placeholder="' . __('Name','wilson') . '" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" />' . '<label for="author">Author</label> ' . ( $req ? '<span class="required">*</span>' : '' ) . '</p>',
+				'<input id="author" name="author" type="text" placeholder="' . __('姓名','wilson') . '" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" />' . '<label for="author">Author</label> ' . ( $req ? '<span class="required">*</span>' : '' ) . '</p>',
 			
 			'email' =>
 				'<p class="comment-form-email">' . '<input id="email" name="email" type="text" placeholder="' . __('Email','wilson') . '" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" /><label for="email">Email</label> ' . ( $req ? '<span class="required">*</span>' : '' ) . '</p>',
 			
 			'url' =>
-			'<p class="comment-form-url">' . '<input id="url" name="url" type="text" placeholder="' . __('Website','wilson') . '" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /><label for="url">Website</label></p>')
+			'<p class="comment-form-url">' . '<input id="url" name="url" type="text" placeholder="' . __('网址','wilson') . '" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /><label for="url">Website</label></p>')
 		),
 	);
 	
