e36a9b2af04588f4109d6ad6fba1c0e0eebba402
Hailong Hao <haohailong@langlib.com>
2016-12-15 15:39:48 +0800

    修正汉化疏漏

--- functions.php
+++ functions.php
@@ -172,7 +172,7 @@ function wilson_meta() { ?>
 
 			<span class="date-sep"> / </span>
 
-			<?php comments_popup_link( '<span class="comment">' . __( '0 Comments', 'wilson' ) . '</span>', __( '1 Comment', 'wilson' ), __( '% Comments', 'wilson' ) ); ?>
+			<?php comments_popup_link( '<span class="comment">' . __( '暂无评论', 'wilson' ) . '</span>', __( '一条评论', 'wilson' ), __( '% 条评论', 'wilson' ) ); ?>
 
 		<?php endif; ?>
 
