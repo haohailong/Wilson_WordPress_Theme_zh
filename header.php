b16a57a58672b184af4b496d4c7b02e69398b0dc
Hailong Hao <haohailong@langlib.com>
2016-12-15 15:38:57 +0800

    Add font-awesome support

--- header.php
+++ header.php
@@ -3,99 +3,100 @@
 <html <?php language_attributes(); ?>>
 
 	<head profile="http://gmpg.org/xfn/11">
-		
+
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
-		 
+		<link rel="stylesheet" href="/wp-content/themes/wilson/font-awesome/css/font-awesome.min.css">
+
 		<?php wp_head(); ?>
-	
+
 	</head>
-	
+
 	<body <?php body_class(); ?>>
-	
+
 		<div class="wrapper">
-	
+
 			<div class="sidebar">
-							
+
 				<div class="blog-header">
-				
+
 					<?php if ( get_theme_mod( 'wilson_logo' ) ) : ?>
-					
+
 						<a class="blog-logo" href='<?php echo home_url(); ?>' title='<?php bloginfo( 'name' ); ?> &mdash; <?php bloginfo( 'description' ); ?>' rel='home'>
 				        	<img src="<?php echo esc_url( get_theme_mod( 'wilson_logo' ) ); ?>" alt="<?php bloginfo( 'name' ); ?>">
 				        </a>
-					
+
 					<?php else : ?>
-				
+
 						<h1 class="blog-title">
 							<a href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?> &mdash; <?php bloginfo( 'description' ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 						</h1>
-						
+
 						<h3 class="blog-description"><?php bloginfo( 'description' ); ?></h3>
-					
+
 					<?php endif; ?>
 
 				</div> <!-- .blog-header -->
-				
+
 				<div class="nav-toggle toggle">
-				
+
 					<p>
 						<span class="show"><?php _e( 'Show menu', 'wilson' ); ?></span>
 						<span class="hide"><?php _e( 'Hide menu', 'wilson' ); ?></span>
 					</p>
-				
+
 					<div class="bars">
-							
+
 						<div class="bar"></div>
 						<div class="bar"></div>
 						<div class="bar"></div>
-						
+
 						<div class="clear"></div>
-						
+
 					</div> <!-- .bars -->
-				
+
 				</div> <!-- .nav-toggle -->
-				
+
 				<div class="blog-menu">
-			
+
 					<ul class="navigation">
-					
+
 						<?php if ( has_nav_menu( 'primary' ) ) {
-																			
-							wp_nav_menu( array( 
-								'container'      => '', 
+
+							wp_nav_menu( array(
+								'container'      => '',
 								'items_wrap'     => '%3$s',
-								'theme_location' => 'primary', 
-								'walker'         => new wilson_nav_walker					
-							) ); 
-                            
+								'theme_location' => 'primary',
+								'walker'         => new wilson_nav_walker
+							) );
+
                         } else {
-						
+
 							wp_list_pages( array(
 								'container' => '',
 								'title_li'  => ''
 							) );
-							
+
 						} ?>
-												
+
 					 </ul> <!-- .navigation -->
-					 
+
 					 <div class="clear"></div>
-					 
+
 				</div> <!-- .blog-menu -->
-				
+
 				<div class="mobile-menu">
-						 
+
 					 <ul class="navigation">
-					
+
 						<?php if ( has_nav_menu( 'primary' ) ) {
-																			
-                            wp_nav_menu( array( 
-                                'container'      => '', 
+
+                            wp_nav_menu( array(
+                                'container'      => '',
                                 'items_wrap'     => '%3$s',
-                                'theme_location' => 'primary', 
+                                'theme_location' => 'primary',
                                 'walker'         => new wilson_nav_walker
-                            ) ); 
+                            ) );
 
                         } else {
 
@@ -103,21 +104,21 @@
                                 'container' => '',
                                 'title_li'  => ''
                             ) );
-							
+
 						} ?>
-						
+
 					 </ul>
-					 
+
 				</div> <!-- .mobile-menu -->
-				
+
 				<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 
 					<div class="widgets" role="complementary">
-					
+
 						<?php dynamic_sidebar( 'sidebar' ); ?>
-						
+
 					</div><!-- .widgets -->
-					
+
 				<?php endif; ?>
-									
+
 			</div> <!-- .sidebar -->
